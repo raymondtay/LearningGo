4166d92fa88627509cca3e1629303445f9fdf733
Raymond Tay <raymondtay1974@gmail.com>
2019-11-06 23:39:06 +0800

    building it up

---
+++ MasteringGo/Chap3/datetime.go
@@ -0,0 +1,21 @@
+package main
+
+import ( 
+"fmt"
+"time"
+)
+
+func main() {
+fmt.Println("Epoch time: " , time.Now().Unix())
+t := time.Now()
+fmt.Println(t, t.Format(time.RFC3339))
+fmt.Println(t.Weekday(), t.Day(), t.Month(), t.Year())
+
+formatT := t.Format("01 March 2006")
+fmt.Println(formatT)
+loc,  _ := time.LoadLocation("Europe/Paris")
+londonTime := t.In(loc)
+fmt.Println("Paris:", londonTime)
+}
+
+
